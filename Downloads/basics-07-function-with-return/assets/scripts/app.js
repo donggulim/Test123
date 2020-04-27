931019656538ef3f3ad84540b64b16c11a6840ce
MacOS_Dani <dani@imdong-guui-MacBookPro.local>
2020-04-27 19:54:39 +0900

    연습

---
+++ Downloads/basics-07-function-with-return/assets/scripts/app.js
@@ -0,0 +1,12 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function add(num1, num2) {
+    const result = num1 + num2;
+    return result;
+const additionResult = add(1,2);
+currentResult = add(1, 2);
+
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+
+outputResult(currentResult, calculationDescription);
